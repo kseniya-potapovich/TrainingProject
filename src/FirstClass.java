2acdc311cca457d2dcf6ececb2fff0e2d4c7ffce
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-10-06 17:01:38 +0300

    A little more

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,5 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("I do first commit here");
         System.out.println("Next commit :)");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Third commit!");
+        }
     }
 }
