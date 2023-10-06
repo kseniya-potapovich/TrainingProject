c0a01f2ce1499f1bee406c4b014cb471eacda40f
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-10-06 16:57:39 +0300

    Next commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("I do first commit here");
+        System.out.println("Next commit :)");
     }
 }
